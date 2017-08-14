03084fd987fb16b19319b7578d20e9c06d180cd2
Diljeet Singh <jeetds1994@gmail.com>
2017-08-14 16:01:06 -0400

    Done.

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,1 +1,21 @@
 // Code Invitation Component Here
+import React from 'react'
+
+class Invitation extends React.Component{
+
+
+  render(){
+    const children = React.Children.map(this.props.children, child => {
+      return ( <div className="some-component-special-class">{child}</div> )
+    })
+    return(
+      <div>
+      <h1>You've been invited!</h1>
+      <p>This component has {React.Children.count(this.props.children)} children.</p>
+       {children}
+      </div>
+    )
+  }
+}
+
+export default Invitation
--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,18 @@
 // Code ThemedDecoration Component Here
+import React from 'react'
+
+class ThemedDecoration extends React.Component{
+
+  render(){
+
+    return(
+      <div>
+        {this.props.children.map((child) => {
+            return <p className={this.props.theme}>{child}</p>
+        })}
+      </div>
+    )
+  }
+}
+
+export default ThemedDecoration
